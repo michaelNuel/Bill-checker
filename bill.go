45b9b99fa8cb9c143e40486f093dd316b1543c59
michaelNuel <michaeltochukwuemmanuel45@gmail.com>
2025-07-17 12:41:34 +0100

    testing push

--- bill.go
+++ bill.go
@@ -55,6 +55,6 @@ func (b *bill) updateTip(tip float64) {
 // add an item to the  bill
 
 
-// func (b *bill) addItem (name string, price float64){
-// 	b.items[name] = price 
-// }
+func (b *bill) addItem (name string, price float64){
+	b.items[name] = price 
+}
